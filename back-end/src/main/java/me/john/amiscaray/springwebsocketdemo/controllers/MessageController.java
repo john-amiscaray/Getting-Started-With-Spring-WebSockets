7c3b0e05fc2b3c98a39c25b644e0b7f2af5f7538
john-amiscaray <john_ernest_amiscaray@outlook.com>
2021-04-01 21:44:46 -0400

    removed picture I accidentally added (whoops again)

--- back-end/src/main/java/me/john/amiscaray/springwebsocketdemo/controllers/MessageController.java
+++ back-end/src/main/java/me/john/amiscaray/springwebsocketdemo/controllers/MessageController.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MessageController {
 
+    // Handles messages from /app/chat. (Note the Spring adds the /app prefix for us).
     @MessageMapping("/chat")
+    // Sends the return value of this method to /topic/messages
     @SendTo("/topic/messages")
     public MessageDto getMessages(MessageDto dto){
 
